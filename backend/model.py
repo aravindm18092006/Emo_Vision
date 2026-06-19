0167264532b052c1a3ce4a66489865b5d5ef602c
aravindm18092006 <aravindm180906@gmail.com>
2026-06-19 12:41:17 +0530

    Scale bounding box coordinates back to original image size

--- backend/model.py
+++ backend/model.py
@@ -12,16 +12,21 @@ class EmotionDetector:
         return self.detector
     
     def detect_emotions(self, image):
-        image = cv2.resize(image, (320, 240))
-        results = self._get_detector().detect_emotions(image)
+        orig_h, orig_w = image.shape[:2]
+        resized = cv2.resize(image, (320, 240))
+        results = self._get_detector().detect_emotions(resized)
         
         if not results:
             return {'faces': []}
         
+        scale_x = orig_w / 320
+        scale_y = orig_h / 240
+        
         faces = []
         for face in results:
             emotions = {k: float(v) for k, v in face['emotions'].items()}
-            box = [int(x) for x in face['box']]
+            x, y, w, h = face['box']
+            box = [int(x * scale_x), int(y * scale_y), int(w * scale_x), int(h * scale_y)]
             dominant_emotion = max(emotions.items(), key=lambda x: x[1])[0]
             faces.append({
                 'box': box,
