695a8dc18d15e791bf35f502cbabf4dac038781b
aravindm18092006 <aravindm180906@gmail.com>
2026-06-19 10:20:43 +0530

    Force CORS headers on all responses, fix gunicorn timeout

--- backend/app.py
+++ backend/app.py
@@ -21,6 +21,13 @@ CORS(app, resources={
     }
 })
 
+@app.after_request
+def add_cors_headers(response):
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Methods'] = 'GET, POST, OPTIONS'
+    response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
+    return response
+
 # Initialize emotion detector
 emotion_detector = EmotionDetector()
 
